85009bddfbbe03bcb0481c77b589da4417d880d9
rstubryan <restubumirr@gmail.com>
2024-06-01 14:25:52 +0700

    feat(+): add main pages

---
+++ src/pages/MainPages.jsx
@@ -0,0 +1,11 @@
+export default function MainPages() {
+  return (
+    <>
+      <div className="">
+        <h1 className="">
+          <i className="bi bi-amd"></i>Main Pages
+        </h1>
+      </div>
+    </>
+  );
+}
