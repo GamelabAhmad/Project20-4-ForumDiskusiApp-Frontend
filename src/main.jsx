0d1002023920550bc71433fd0903935548e9732f
rstubryan <restubumirr@gmail.com>
2024-06-01 15:03:10 +0700

    feat(+): add more routing to new pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,26 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainPages from "./pages/MainPages.jsx";
+import QuestionPages from "./pages/QuestionPages.jsx";
+import ForumPages from "./pages/ForumPages.jsx";
+import TopicPages from "./pages/TopicPages.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainPages />,
+  },
+  {
+    path: "/question",
+    element: <QuestionPages />,
+  },
+  {
+    path: "/forum",
+    element: <ForumPages />,
+  },
+  {
+    path: "/topic",
+    element: <TopicPages />,
   },
 ]);
 
