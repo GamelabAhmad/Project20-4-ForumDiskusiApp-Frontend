5ff582404f9188a2a8f4a3bdc5364bb9435d91bd
ramseccc <ramadhanahasan0107@gmail.com>
2024-06-01 22:25:46 +0700

    feat(+): add SubheadingText

--- src/components/atoms/SubheadingText/index.jsx
+++ src/components/atoms/SubheadingText/index.jsx
@@ -1,3 +1,9 @@
-export default function SubheadingText() {
-  return <>pake h3</>;
-}
+import React from "react";
+
+export default function SubheadingText({children, className, ...props}) {
+  return <>
+  <p className={`fw-semibold fs-3 lh-sm text-uppercase text-start text-black mx-2 py-5 ${className}`} {...props}>
+  {children}
+  </p>
+  </>
+};
