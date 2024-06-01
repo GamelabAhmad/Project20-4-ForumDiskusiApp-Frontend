72ada3da339ccbfa0d2288679fa3fcd72dda5407
ramseccc <ramadhanahasan0107@gmail.com>
2024-06-01 22:27:09 +0700

    feat(+): add TypographyText

--- src/components/atoms/TypographyText/index.jsx
+++ src/components/atoms/TypographyText/index.jsx
@@ -1,3 +1,9 @@
-export default function TypographyText() {
-  return <>pake p</>;
-}
+import React from "react";
+
+export default function TypographyText({children, className, ...props}) {
+  return <>
+  <p className={`fw-normal fs-6 lh-sm text-start text-black py-5 ${className}`} {...props}>
+  {children}
+  </p>
+  </>
+};
