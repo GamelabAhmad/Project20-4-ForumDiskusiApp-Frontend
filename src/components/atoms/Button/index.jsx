c97a7128b12e536388c84bb942f0319c1277e15a
rstubryan <restubumirr@gmail.com>
2024-06-01 21:35:23 +0700

    refactor(atoms/button): refactor button

--- src/components/atoms/Button/index.jsx
+++ src/components/atoms/Button/index.jsx
@@ -1,13 +1,9 @@
-import React from "react";
-import { Button1 } from "react-bootstrap";
-
-const Button = (props) => {
-  const { children, classname = "bg-black" } = props;
+export default function Button({ className, type, children, ...props }) {
   return (
-    <Button1 className={`h-10 px-7 font-bold rounded-md mt-6 ${classname} text-white`} type="submit">
-      {children}
-    </Button1>
+    <>
+      <button className={`btn btn-primary ${className}`} type={type} {...props}>
+        {children}
+      </button>
+    </>
   );
-};
-
-export default Button;
+}
