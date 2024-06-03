419390e22ff88b54653fff332cef04f99f1de24b
josuafrino <josuafrino@gmail.com>
2024-06-03 13:10:32 +0700

    "fix(+):fix the code"

--- src/components/molecules/Sidebar/index.jsx
+++ src/components/molecules/Sidebar/index.jsx
@@ -6,23 +6,23 @@ import IconPlaceholder from "../../atoms/IconPlaceholder";
 const Sidebar = () => {
   return (
     <div>
-      <Container className="col-sm-3  border border-dark rounded">
-        <ul className="flex mb-1  list-unstyled ">
+      <Container className="col-sm-3  border border-dark rounded p-2">
+        <ul className="pt-2 ps-4 list-unstyled ">
           <li className="mb-2">
             <IconPlaceholder className="ps-2  text-dark bi bi-house-door" />
-            <TypographyText className="d-inline ps-4 ms-4 text-center">HOME</TypographyText>
+            <TypographyText className="d-inline ps-4 ms-4 ">HOME</TypographyText>
           </li>
           <li className="mb-2">
             <IconPlaceholder className="ps-2  text-dark bi bi-globe2" />
-            <TypographyText className="d-inline  ps-3 ms-4 text-center">QUESTION</TypographyText>
+            <TypographyText className="d-inline  ps-3 ms-4 ">QUESTION</TypographyText>
           </li>
           <li className="mb-2">
             <IconPlaceholder className="ps-2  text-dark bi bi-people" />
-            <TypographyText className="d-inline  ps-4 ms-4 text-center">FORUM</TypographyText>
+            <TypographyText className="d-inline  ps-4 ms-4 ">FORUM</TypographyText>
           </li>
           <li className="mb-2">
             <IconPlaceholder className="ps-2  text-dark bi bi-journals" />
-            <TypographyText className="d-inline ps-4 ms-4 text-center">TOPIC</TypographyText>
+            <TypographyText className="d-inline ps-4 ms-4 ">TOPIC</TypographyText>
           </li>
         </ul>
       </Container>
