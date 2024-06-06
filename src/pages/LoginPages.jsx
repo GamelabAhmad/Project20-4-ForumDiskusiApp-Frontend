e8bd19eacd70c246ffdfc092b0540614b53ae76c
rstubryan <restubumirr@gmail.com>
2024-06-06 10:59:22 +0700

    feat(pages): add auth pages

---
+++ src/pages/LoginPages.jsx
@@ -0,0 +1,17 @@
+import AuthLayout from "../components/templates/AuthLayout.jsx";
+import LoginForm from "../components/organisms/LoginForm/index.jsx";
+import Card from "../components/molecules/Card/index.jsx";
+
+export default function LoginPages() {
+  return (
+    <>
+      <div className="d-flex align-items-center justify-content-center min-vh-100">
+        <Card className="w-75 w-sm-75 w-md-50 w-lg-50 w-xl-50 w-xxl-25 p-4 rounded-4">
+          <AuthLayout label="Login">
+            <LoginForm />
+          </AuthLayout>
+        </Card>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/RegisterPages.jsx
@@ -0,0 +1,17 @@
+import AuthLayout from "../components/templates/AuthLayout.jsx";
+import RegisterForm from "../components/organisms/RegisterForm/index.jsx";
+import Card from "../components/molecules/Card/index.jsx";
+
+export default function RegisterPages() {
+  return (
+    <>
+      <div className="d-flex align-items-center justify-content-center min-vh-100 p-4 p-sm-0">
+        <Card className="w-100 w-sm-75 w-md-50 w-lg-50 w-xl-50 w-xxl-25 p-2 p-sm-4 rounded-4">
+          <AuthLayout label="Register">
+            <RegisterForm />
+          </AuthLayout>
+        </Card>
+      </div>
+    </>
+  );
+}
