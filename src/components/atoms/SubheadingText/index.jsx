c5e2459645a82df0c2cc08306881cda4423ade37
rstubryan <restubumirr@gmail.com>
2024-06-06 17:48:00 +0700

    refactor(components/atoms): add css reset

--- src/components/atoms/SubheadingText/index.jsx
+++ src/components/atoms/SubheadingText/index.jsx
@@ -1,7 +1,20 @@
-export default function SubheadingText({ children, className, ...props }) {
+import useCssReset from "../../../hooks/useCssReset.js";
+
+export default function SubheadingText({
+  cssReset,
+  children,
+  className,
+  ...props
+}) {
+  const style = useCssReset(cssReset);
+
   return (
     <>
-      <p className={`fw-normal fs-3 lh-sm text-black ${className}`} {...props}>
+      <p
+        className={`fw-normal fs-3 lh-sm text-black ${className}`}
+        style={style}
+        {...props}
+      >
         {children}
       </p>
     </>
