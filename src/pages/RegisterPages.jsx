b1e8532249d946aaafef17d03dc508c151bd119d
rstubryan <restubumirr@gmail.com>
2024-06-07 23:05:14 +0700

    feat(+): add validation and utilize breakpoint

--- src/pages/RegisterPages.jsx
+++ src/pages/RegisterPages.jsx
@@ -3,10 +3,15 @@ import RegisterForm from "../components/organisms/RegisterForm/index.jsx";
 import Card from "../components/molecules/Card/index.jsx";
 
 export default function RegisterPages() {
+  const token = localStorage.getItem("token");
+  if (token !== "null") {
+    window.location.href = "/";
+  }
+
   return (
     <>
       <div className="d-flex align-items-center justify-content-center min-vh-100 p-4 p-sm-0">
-        <Card className="w-100 w-sm-75 w-md-50 w-lg-50 w-xl-50 w-xxl-25 p-2 p-sm-4 rounded-4">
+        <Card className="w-100 w-sm-75 w-xl-50 w-xxl-25 p-2 p-sm-4 rounded-4">
           <AuthLayout label="Register">
             <RegisterForm />
           </AuthLayout>
