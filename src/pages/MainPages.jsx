8930253893595f4f5416d657f73f12574eae78b3
rstubryan <restubumirr@gmail.com>
2024-06-07 23:08:38 +0700

    feat(+): add templates for main pages

--- src/pages/MainPages.jsx
+++ src/pages/MainPages.jsx
@@ -1,25 +1,9 @@
-import { NavLink } from "react-router-dom";
+import MainPagesLayout from "../components/templates/MainPagesLayout.jsx";
 
 export default function MainPages() {
   return (
     <>
-      <h1 className="">Main Pages</h1>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to={"/"}>Home</NavLink>
-          </li>
-          <li>
-            <NavLink to={"/forum"}>Forum</NavLink>
-          </li>
-          <li>
-            <NavLink to={"/question"}>Question</NavLink>
-          </li>
-          <li>
-            <NavLink to={"/topic"}>Topic</NavLink>
-          </li>
-        </ul>
-      </nav>
+      <MainPagesLayout />
     </>
   );
 }
