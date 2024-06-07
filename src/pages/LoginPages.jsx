428628bc48b90b03dd61149988fae7a2e0804c4a
rstubryan <restubumirr@gmail.com>
2024-06-07 23:08:04 +0700

    feat(+): add validation

--- src/pages/LoginPages.jsx
+++ src/pages/LoginPages.jsx
@@ -3,6 +3,11 @@ import LoginForm from "../components/organisms/LoginForm/index.jsx";
 import Card from "../components/molecules/Card/index.jsx";
 
 export default function LoginPages() {
+  const token = localStorage.getItem("token");
+  if (token !== "null") {
+    window.location.href = "/";
+  }
+
   return (
     <>
       <div className="d-flex align-items-center justify-content-center min-vh-100">
