c8bc424b2e3a14f83937abb1abb8d20e5a97682a
rstubryan <restubumirr@gmail.com>
2024-06-08 10:00:54 +0700

    refactor(components/organisms): card header utilization on card usages and button validation

--- src/components/organisms/CardHeader/index.jsx
+++ src/components/organisms/CardHeader/index.jsx
@@ -1,19 +1,42 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Card from "../../molecules/Card/index.jsx";
 import Button from "../../atoms/Button/index.jsx";
-import { Link } from "react-router-dom";
+import Toasts from "../../molecules/Toasts/index.jsx";
 
 export default function CardHeader({ title, description }) {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+  const [showToast, setShowToast] = useState(false);
+
+  const handleAskQuestionClick = (e) => {
+    if (token === "null" || !token) {
+      e.preventDefault();
+      setShowToast(true);
+      setTimeout(() => setShowToast(false), 10000);
+    } else {
+      navigate("/dashboard");
+    }
+  };
+
   return (
-    <>
-      <Card className="shadow-sm p-3">
-        <div className="d-flex justify-content-between mb-3 mb-lg-0">
-          <Card.Title className="fw-semibold text-primary">{title}</Card.Title>
-          <Link to={"/dashboard"}>
-            <Button variant={"primary"}>Ask Question</Button>
-          </Link>
-        </div>
-        <Card.Description className="mb-5">{description}</Card.Description>
-      </Card>
-    </>
+    <Card className="shadow-sm p-3">
+      <div className="d-flex justify-content-between mb-3 mb-lg-0">
+        <Card.Title className="fw-semibold text-primary">{title}</Card.Title>
+        <Button variant="primary" onClick={handleAskQuestionClick}>
+          Ask Question
+        </Button>
+      </div>
+      <Card.Description className="mb-5">{description}</Card.Description>
+
+      {showToast && (
+        <Toasts
+          variant={"danger"}
+          variantBody={"danger-subtle"}
+          title={"Warning"}
+          description={"You need to login to ask a question."}
+        />
+      )}
+    </Card>
   );
 }
