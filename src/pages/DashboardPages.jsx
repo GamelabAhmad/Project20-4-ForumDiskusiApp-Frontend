d4d5cbaed29eff470bee22c08d2e8310df394221
rstubryan <restubumirr@gmail.com>
2024-06-08 22:48:10 +0700

    feat(pages): add validation token

--- src/pages/DashboardPages.jsx
+++ src/pages/DashboardPages.jsx
@@ -1,6 +1,12 @@
 import DashboardPagesLayout from "../components/templates/DashboardPagesLayout.jsx";
 
 export default function DashboardPages() {
+  const token = localStorage.getItem("token");
+  if (token === "null") {
+    window.location.href = "/";
+    return null;
+  }
+
   return (
     <>
       <DashboardPagesLayout />
