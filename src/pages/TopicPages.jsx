6f5ca9d1fa9b9b41d65dddd4e07fb135be88b5d9
rstubryan <restubumirr@gmail.com>
2024-06-08 21:31:57 +0700

    feat(topic): add topic pages

--- src/pages/TopicPages.jsx
+++ src/pages/TopicPages.jsx
@@ -1,7 +1,9 @@
+import TopicPagesLayout from "../components/templates/TopicPagesLayout.jsx";
+
 export default function TopicPages() {
   return (
     <>
-      <h1>Topic Pages</h1>
+      <TopicPagesLayout />
     </>
   );
 }
