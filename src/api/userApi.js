4aec1c027264a05aa189cc2fbe553a8daed2fedd
rstubryan <restubumirr@gmail.com>
2024-06-09 21:09:49 +0700

    feat(user): add profile

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-export async function getUserByUsername(username) {
+export async function getUserByUsername(id) {
   try {
     const response = await axios({
       method: "get",
-      url: `http://localhost:3000/${username}`,
+      url: `http://localhost:3000/profile/${id}`,
       headers: { "Content-Type": "application/json" },
     });
     return response.data;
