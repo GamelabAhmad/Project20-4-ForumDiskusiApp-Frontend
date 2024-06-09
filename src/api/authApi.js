1a6c2598c90a7d84e0d6819c923a6c1c0566b9ed
rstubryan <restubumirr@gmail.com>
2024-06-09 23:12:56 +0700

    feat(cookies): add auth cookies

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,36 +1,23 @@
 import axios from "axios";
 
-export async function signIn(username, password) {
-  try {
-    const response = await axios({
-      method: "post",
-      url: "http://localhost:3000/signin",
-      headers: { "Content-Type": "application/json" },
-      data: {
-        username,
-        password,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-}
+const instance = axios.create({
+  baseURL: "http://localhost:3000",
+  withCredentials: true,
+});
 
-export async function signUp(username, email, password) {
-  try {
-    const response = await axios({
-      method: "post",
-      url: "http://localhost:3000/signup",
-      headers: { "Content-Type": "application/json" },
-      data: {
-        username,
-        email,
-        password,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-}
+export const signIn = async (username, password) => {
+  const response = await instance.post("/signin", {
+    username,
+    password,
+  });
+  return response.data;
+};
+
+export const signUp = async (username, email, password) => {
+  const response = await instance.post("/signup", {
+    username,
+    email,
+    password,
+  });
+  return response.data;
+};
