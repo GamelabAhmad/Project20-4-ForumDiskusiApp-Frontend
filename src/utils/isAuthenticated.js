3dc2145d0d4726bd92b03f8c4975cef62ccb2143
rstubryan <restubumirr@gmail.com>
2024-06-09 21:02:39 +0700

    feat(auth): check user if auth

--- src/utils/isAuthenticated.js
+++ src/utils/isAuthenticated.js
@@ -1,7 +1,12 @@
 export const isAuthenticated = () => {
   const token = localStorage.getItem("token");
+  const user = localStorage.getItem("user");
 
   if (!token) {
     localStorage.setItem("token", "null");
+  }
+
+  if (!user) {
+    localStorage.setItem("user", "null");
   }
 };
