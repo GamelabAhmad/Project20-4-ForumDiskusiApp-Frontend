df95b0bfe0f315db9c7b300c9f070db5970e304c
rstubryan <restubumirr@gmail.com>
2024-06-09 21:07:14 +0700

    feat(auth): welcome pages auth

--- src/components/templates/DashboardPagesLayout.jsx
+++ src/components/templates/DashboardPagesLayout.jsx
@@ -1,7 +1,27 @@
+import PagesLayout from "./PagesLayout.jsx";
+import ContainerLayout from "./ContainerLayout.jsx";
+import HeadingText from "../atoms/HeadingText/index.jsx";
+import TypographyText from "../atoms/TypographyText/index.jsx";
+
 export default function DashboardPagesLayout() {
+  const user = localStorage.getItem("user");
+
   return (
     <>
-      <h1>Dashboard Pages Layout</h1>
+      <PagesLayout>
+        <ContainerLayout>
+          <HeadingText
+            cssReset={true}
+            className="text-center fw-semibold text-primary"
+          >
+            Dashboard
+          </HeadingText>
+          <TypographyText cssReset={true} className="text-center">
+            Welcome to your dashboard {user}! Here you can view your recent
+            activities and manage your account.
+          </TypographyText>
+        </ContainerLayout>
+      </PagesLayout>
     </>
   );
 }
