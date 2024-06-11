03e1e5b63d1069c075ad4f2fe7f8e16fc308ffc8
rstubryan <restubumirr@gmail.com>
2024-06-11 15:54:08 +0700

    feat(user-profile): add user profile pages

--- src/components/templates/UserProfilePagesLayout.jsx
+++ src/components/templates/UserProfilePagesLayout.jsx
@@ -1,35 +1,118 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"; // new
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import { getUserByUsername } from "../../api/userApi.js";
+import PagesLayout from "./PagesLayout.jsx";
+import ContainerLayout from "./ContainerLayout.jsx";
+import HeadingText from "../atoms/HeadingText/index.jsx";
+import AvatarPlaceHolder from "../atoms/AvatarPlaceholder/index.jsx";
+import { getQuestionByUser } from "../../api/questionApi.js";
+import TypographyText from "../atoms/TypographyText/index.jsx";
+import Card from "../molecules/Card/index.jsx";
+import CardPost from "../organisms/CardPost/index.jsx";
+import Button from "../atoms/Button/index.jsx";
+import { getCommentsByPostId } from "../../api/commentApi.js";
 
 export default function UserProfilePagesLayout() {
+  const [questions, setQuestions] = useState([]);
   const [user, setUser] = useState(null);
+  const [comments, setComments] = useState({});
   const { id } = useParams();
 
   useEffect(() => {
-    async function fetchUsername() {
+    async function fetchData() {
       try {
-        const user = await getUserByUsername(id);
-        setUser(user);
+        const fetchedUser = await getUserByUsername(id);
+        setUser(fetchedUser);
+        const comments = {};
+        for (let question of questions) {
+          comments[question.uuid] = await getCommentsByPostId(question.uuid);
+        }
+        setComments(comments);
+        if (fetchedUser) {
+          const fetchedQuestions = await getQuestionByUser(fetchedUser.uuid);
+          setQuestions(fetchedQuestions);
+        }
       } catch (error) {
-        console.error("Error fetching user:", error);
+        console.error("Error fetching data:", error);
       }
     }
 
-    fetchUsername();
-  }, [id]);
+    fetchData();
+  }, [id, questions]);
 
   return (
-    <>
-      {user ? (
-        <div key={user.uuid}>
-          <h1>{user.username}</h1>
-        </div>
-      ) : (
-        <div>
-          <h1>User not found</h1>
-        </div>
-      )}
-    </>
+    <PagesLayout>
+      <ContainerLayout>
+        {user ? (
+          <>
+            <Card key={user.uuid} className="p-3 mb-3">
+              <div className="d-md-flex justify-content-between">
+                <div className="d-grid d-md-flex gap-4 text-center text-md-start">
+                  <AvatarPlaceHolder
+                    src={user.avatar}
+                    alt={user.username}
+                    heightAvatar={150}
+                    widthAvatar={150}
+                    className="border border-2 rounded-circle border-primary img-fluid mx-auto mx-md-0"
+                  />
+                  <div>
+                    <Card.Title className="fw-semibold text-primary">
+                      {user.username}
+                    </Card.Title>
+                    <Card.Description className="fw-lighter">
+                      <TypographyText>{user.follower} Followers</TypographyText>
+                    </Card.Description>
+                    <Card.Description className="fw-lighter mb-3 mb-md-0">
+                      {user.bio ? user.bio : "No bio available"}
+                    </Card.Description>
+                  </div>
+                </div>
+                <div className="">
+                  <Button variant={"primary"} className="w-100 w-md-auto">
+                    Follow
+                  </Button>
+                </div>
+              </div>
+            </Card>
+            {questions.length > 0 ? (
+              questions.map((question) => (
+                <CardPost
+                  key={question.uuid}
+                  title={
+                    <Link
+                      to={`/question/${question.uuid}`}
+                      className="text-decoration-none"
+                    >
+                      {question.title}
+                    </Link>
+                  }
+                  description={question.body}
+                  createdAt={new Date(question.createdAt).toLocaleString()}
+                  username={question.createdBy.username}
+                  avatarSrc={question.createdBy.avatar}
+                  avatarAlt={question.createdBy.username}
+                  votes={question.votes || 0}
+                  answers={
+                    comments[question.uuid] ? comments[question.uuid].length : 0
+                  }
+                  views={question.views || 0}
+                  showImage={false}
+                  showButtons={false}
+                  className={"mb-3"}
+                />
+              ))
+            ) : (
+              <Card>
+                <Card.Title className="d-flex align-items-center justify-content-center fw-semibold">
+                  No posts available
+                </Card.Title>
+              </Card>
+            )}
+          </>
+        ) : (
+          <HeadingText>User not found</HeadingText>
+        )}
+      </ContainerLayout>
+    </PagesLayout>
   );
 }
