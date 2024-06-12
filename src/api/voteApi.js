a251952c5c28214fe718dced5f1a3f2bd23aff6d
rstubryan <restubumirr@gmail.com>
2024-06-12 21:55:22 +0700

    feat(votes): implementation upvotes

---
+++ src/api/voteApi.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+export async function voteQuestion(id) {
+  const token = Cookies.get("jwt");
+
+  try {
+    const response = await axios({
+      method: "post",
+      url: `http://localhost:3000/vote/${id}`,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getVotes(id) {
+  try {
+    const response = await axios({
+      method: "get",
+      url: `http://localhost:3000/votes/${id}`,
+      headers: { "Content-Type": "application/json" },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
--- src/components/organisms/CardPost/index.jsx
+++ src/components/organisms/CardPost/index.jsx
@@ -4,11 +4,13 @@ import Button from "../../atoms/Button/index.jsx";
 import IconPlaceholder from "../../atoms/IconPlaceholder/index.jsx";
 import UserPostDate from "../../molecules/UserPostDate/index.jsx";
 import UserPostSummary from "../../molecules/UserPostSummary/index.jsx";
+import { useParams } from "react-router-dom";
+import { getVotes, voteQuestion } from "../../../api/voteApi.js";
+import { useEffect, useState } from "react";
 
 export default function CardPost({
   votes,
   answers,
-  views,
   title,
   description,
   imageSrc,
@@ -22,12 +24,46 @@ export default function CardPost({
   className,
   ...props
 }) {
+  const { id } = useParams();
+  const [upvoteSuccessful, setUpvoteSuccessful] = useState(false);
+
+  useEffect(() => {
+    async function checkVoteStatus() {
+      try {
+        const response = await getVotes(id);
+        if (response.role === "VOTE") {
+          setUpvoteSuccessful(true);
+        } else {
+          setUpvoteSuccessful(false);
+        }
+      } catch (error) {
+        console.error("Error checking vote status:", error);
+      }
+    }
+
+    checkVoteStatus();
+  }, [id]);
+
+  const handleUpvote = async () => {
+    try {
+      const response = await voteQuestion(id);
+      console.log("Upvoted question:", response);
+      if (response.role === "VOTE") {
+        setUpvoteSuccessful(true);
+      } else if (response.message === "VOTE removed") {
+        setUpvoteSuccessful(false);
+      }
+    } catch (error) {
+      console.error("Error upvoting question:", error);
+    }
+  };
+
   return (
     <>
       <Card className={`shadow-sm p-3 ${className}`} {...props}>
         <div className="row">
           <div className="col-sm-12 col-lg-2 text-start text-lg-end d-flex d-lg-block gap-3 gap-lg-0 mb-3 mb-lg-0">
-            <UserPostSummary votes={votes} answers={answers} views={views} />
+            <UserPostSummary votes={votes} answers={answers} />
           </div>
           <div className="col-12 col-lg-10">
             <Card.Title className="text-primary">
@@ -52,8 +88,9 @@ export default function CardPost({
                 <>
                   <div className={`d-flex gap-3 col-12 col-md-6 mb-3 mb-md-0`}>
                     <Button
-                      variant={"primary"}
+                      variant={upvoteSuccessful ? "success" : "primary"}
                       className={"w-100 w-md-auto rounded-3"}
+                      onClick={handleUpvote}
                     >
                       <IconPlaceholder variant={"arrow-up"} />
                       Upvote
