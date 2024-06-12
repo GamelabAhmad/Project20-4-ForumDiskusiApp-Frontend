a80830068275128fd41d97375779d630bd0393ba
rstubryan <restubumirr@gmail.com>
2024-06-12 22:00:19 +0700

    feat(topics): new landing pages concept

--- src/components/templates/MainPagesLayout.jsx
+++ src/components/templates/MainPagesLayout.jsx
@@ -1,33 +1,11 @@
-import React, { useEffect, useState } from "react";
 import Sidebar from "../molecules/Sidebar/index.jsx";
 import Card from "../molecules/Card/index.jsx";
 import ContainerLayout from "./ContainerLayout.jsx";
 import PagesLayout from "./PagesLayout.jsx";
 import CardHeader from "../organisms/CardHeader/index.jsx";
 import CardPost from "../organisms/CardPost/index.jsx";
-import SkeletonPlaceholder from "../atoms/SkeletonPlaceholder/index.jsx";
-import { getQuestions } from "../../api/questionApi.js";
-import { Link } from "react-router-dom";
 
 export default function MainPagesLayout() {
-  const [userPosts, setUserPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function fetchQuestions() {
-      try {
-        const questions = await getQuestions();
-        setUserPosts(questions);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching questions:", error);
-        setLoading(false);
-      }
-    }
-
-    fetchQuestions();
-  }, []);
-
   return (
     <>
       <PagesLayout>
@@ -42,99 +20,21 @@ export default function MainPagesLayout() {
               <CardHeader
                 title={"Welcome to Twenties!"}
                 description={
-                  "lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                  "Twenties is are a website of Forum Discussion App where you can ask questions and share your knowledge with others. You can also follow other users, like their questions and answers, and comment on them."
                 }
                 buttonTitle={"Ask a Question"}
                 toastsMessage={"ask a question"}
               />
             </div>
-            <div className="col-12 col-lg-10 ms-auto mb-4">
-              {loading ? (
-                <>
-                  <CardPost
-                    title={
-                      <SkeletonPlaceholder
-                        variant={"secondary"}
-                        className={"col-12"}
-                      />
-                    }
-                    description={
-                      <>
-                        <SkeletonPlaceholder
-                          variant={"body-tertiary"}
-                          className={"col-12"}
-                        />
-                        <SkeletonPlaceholder
-                          variant={"secondary"}
-                          className={"col-12"}
-                        />
-                        <SkeletonPlaceholder
-                          variant={"body-tertiary"}
-                          className={"col-12"}
-                        />
-                      </>
-                    }
-                    showImage={false}
-                    showButtons={false}
-                    votes={
-                      <>
-                        <SkeletonPlaceholder
-                          variant={"secondary"}
-                          className={"col-12 col-lg-4"}
-                        />
-                      </>
-                    }
-                    answers={
-                      <>
-                        <SkeletonPlaceholder
-                          variant={"body-tertiary"}
-                          className={"col-12 col-lg-4"}
-                        />
-                      </>
-                    }
-                    views={
-                      <>
-                        <SkeletonPlaceholder
-                          variant={"secondary"}
-                          className={"col-12 col-lg-4"}
-                        />
-                      </>
-                    }
-                    className={"placeholder-glow mb-3"}
-                  />
-                </>
-              ) : userPosts.length > 0 ? (
-                userPosts.map((post) => (
-                  <CardPost
-                    key={post.uuid}
-                    title={
-                      <Link
-                        to={`/question/${post.uuid}`}
-                        className="text-decoration-none"
-                      >
-                        {post.title}
-                      </Link>
-                    }
-                    description={post.body}
-                    createdAt={new Date(post.createdAt).toLocaleString()}
-                    username={post.createdBy.username}
-                    avatarSrc={post.createdBy.avatar}
-                    avatarAlt={post.createdBy.username}
-                    votes={post.votes || 0}
-                    answers={post.answers || 0}
-                    views={post.views || 0}
-                    showImage={false}
-                    showButtons={false}
-                    className={"mb-3"}
-                  />
-                ))
-              ) : (
-                <Card>
-                  <Card.Title className="d-flex align-items-center justify-content-center fw-semibold">
-                    No posts available
-                  </Card.Title>
-                </Card>
-              )}
+            <div className="col-12 col-lg-10 ms-auto mb-3">
+              <CardPost>
+                title={"Latest Questions"}
+                description={"Here are the latest questions from our users"}
+                author={"Twenties"}
+                date={"2021-09-01"}
+                votes={"10"}
+                comments={"5"}
+              </CardPost>
             </div>
           </div>
         </ContainerLayout>
