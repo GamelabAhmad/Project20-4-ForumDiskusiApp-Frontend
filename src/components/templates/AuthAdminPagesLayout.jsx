6a7b65927d2e77d6576d9dd44fd0526ec3bab7cc
rstubryan <restubumirr@gmail.com>
2024-06-13 00:19:25 +0700

    feat(auth-admin-pages): new concept design for admin pages

--- src/components/templates/AuthAdminPagesLayout.jsx
+++ src/components/templates/AuthAdminPagesLayout.jsx
@@ -1,14 +1,12 @@
 import PagesLayout from "./PagesLayout.jsx";
 import ContainerLayout from "./ContainerLayout.jsx";
-import AuthTopicForm from "../organisms/AuthTopicForm/index.jsx";
-import AuthForumForm from "../organisms/AuthForumForm/index.jsx";
 import HeadingText from "../atoms/HeadingText/index.jsx";
 import Cookies from "js-cookie";
-import Card from "../molecules/Card/index.jsx";
-import SubheadingText from "../atoms/SubheadingText/index.jsx";
 import AuthTableAdminForum from "../organisms/AuthTableAdminForum/index.jsx";
 import AuthTableAdminTopic from "../organisms/AuthTableAdminTopic/index.jsx";
 import AuthTableAdminQuestion from "../organisms/AuthTableAdminQuestion/index.jsx";
+import TypographyText from "../atoms/TypographyText/index.jsx";
+import IconPlaceholder from "../atoms/IconPlaceholder/index.jsx";
 
 export default function AuthAdminPagesLayout() {
   const user = Cookies.get("user");
@@ -23,21 +21,65 @@ export default function AuthAdminPagesLayout() {
               children={"Admin Page"}
               className="fw-semibold text-primary text-center my-3"
             />
-            <AuthTableAdminQuestion />
-            <AuthTableAdminForum />
-            <AuthTableAdminTopic />
-            <Card className="p-4 mb-3">
-              <SubheadingText className="text-primary">
-                Topic Form
-              </SubheadingText>
-              <AuthTopicForm />
-            </Card>
-            <Card className="p-4 mb-3">
-              <SubheadingText className="text-primary">
-                Forum Form
-              </SubheadingText>
-              <AuthForumForm />
-            </Card>
+            <div className="py-3">
+              <TypographyText
+                cssReset={true}
+                className="text-center alert alert-primary"
+                role="alert"
+              >
+                <IconPlaceholder variant={"info-circle"} className="me-2" />
+                Welcome to your dashboard{" "}
+                <span className="text-primary">{user}</span>! Here you can view
+                your recent activities.
+              </TypographyText>
+            </div>
+            <div className="d-flex flex-column align-items-center">
+              <button
+                className="btn btn-primary my-3"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseQuestion"
+                aria-expanded="false"
+                aria-controls="collapseQuestion"
+              >
+                Toggle Question Table
+              </button>
+              <div className="collapse w-100" id="collapseQuestion">
+                <div className="card card-body table-responsive">
+                  <AuthTableAdminQuestion />
+                </div>
+              </div>
+              <button
+                className="btn btn-primary my-3"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseForum"
+                aria-expanded="false"
+                aria-controls="collapseForum"
+              >
+                Toggle Forum Table
+              </button>
+              <div className="collapse w-100" id="collapseForum">
+                <div className="card card-body table-responsive">
+                  <AuthTableAdminForum />
+                </div>
+              </div>
+              <button
+                className="btn btn-primary my-3"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseTopic"
+                aria-expanded="false"
+                aria-controls="collapseTopic"
+              >
+                Toggle Topic Table
+              </button>
+              <div className="collapse w-100 mb-3" id="collapseTopic">
+                <div className="card card-body table-responsive">
+                  <AuthTableAdminTopic />
+                </div>
+              </div>
+            </div>
           </ContainerLayout>
         </PagesLayout>
       </>
--- src/components/templates/CreateEditQuestionForumTopicPagesLayout.jsx
+++ src/components/templates/CreateEditQuestionForumTopicPagesLayout.jsx
@@ -5,9 +5,13 @@ import HeadingText from "../atoms/HeadingText/index.jsx";
 import Button from "../atoms/Button/index.jsx";
 import IconPlaceholder from "../atoms/IconPlaceholder/index.jsx";
 import { Link } from "react-router-dom";
-import EditForm from "../organisms/EditForm/index.jsx";
+import EditForm from "../organisms/EditQuestionForm/index.jsx";
+import AuthCreateForumForm from "../organisms/AuthCreateForumForm/index.jsx";
+import AuthCreateTopicForm from "../organisms/AuthCreateTopicForm/index.jsx";
+import AuthEditForumForm from "../organisms/AuthEditForumForm/index.jsx";
+import AuthEditTopicForm from "../organisms/AuthEditTopicForm/index.jsx";
 
-export default function CreateEditQuestionPagesLayout({ title }) {
+export default function CreateEditQuestionForumTopicPagesLayout({ title }) {
   return (
     <>
       <PagesLayout>
@@ -23,7 +27,12 @@ export default function CreateEditQuestionPagesLayout({ title }) {
           >
             {title}
           </HeadingText>
-          {title === "Ask a Question" ? <QuestionForm /> : <EditForm />}
+          {title === "Ask a Question" && <QuestionForm />}
+          {title === "Edit a Question" && <EditForm />}
+          {title === "Create a Forum" && <AuthCreateForumForm />}
+          {title === "Create a Topic" && <AuthCreateTopicForm />}
+          {title === "Edit a Forum" && <AuthEditForumForm />}
+          {title === "Edit a Topic" && <AuthEditTopicForm />}
         </ContainerLayout>
       </PagesLayout>
     </>
