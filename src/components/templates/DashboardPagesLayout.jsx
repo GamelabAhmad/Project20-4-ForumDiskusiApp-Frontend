d9fa334c8d1c4d9a2aa99949cd63d8001a287acc
rstubryan <restubumirr@gmail.com>
2024-06-14 19:02:06 +0700

    feat(!profile): edit profile button

--- src/components/templates/DashboardPagesLayout.jsx
+++ src/components/templates/DashboardPagesLayout.jsx
@@ -74,6 +74,17 @@ export default function DashboardPagesLayout() {
               your recent activities.
             </TypographyText>
           </div>
+          <div className="mt-1 mb-3">
+            <Link to={`/profile/${user}/edit`} className="text-decoration-none">
+              <Button
+                variant={"primary"}
+                className="btn-sm d-flex gap-2 rounded-3"
+              >
+                <IconPlaceholder variant={"pencil"} />
+                Edit Profile
+              </Button>
+            </Link>
+          </div>
           <div className="d-flex justify-content-between align-items-center mb-3">
             <SubheadingText cssReset={true} className="fw-semibold ">
               Question Management
