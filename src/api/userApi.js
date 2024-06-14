e399af567fed3467995a363f923d8c9ebae59043
rstubryan <restubumirr@gmail.com>
2024-06-14 19:05:21 +0700

    feat(!update-profile): update profile axios wip

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 export async function getUserByUsername(id) {
   try {
@@ -6,6 +7,25 @@ export async function getUserByUsername(id) {
       method: "get",
       url: `http://localhost:3000/profile/${id}`,
       headers: { "Content-Type": "application/json" },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function updateUser(id, data) {
+  const token = Cookies.get("jwt");
+
+  try {
+    const response = await axios({
+      method: "put",
+      url: `http://localhost:3000/setting/${id}`,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      data,
     });
     return response.data;
   } catch (error) {
