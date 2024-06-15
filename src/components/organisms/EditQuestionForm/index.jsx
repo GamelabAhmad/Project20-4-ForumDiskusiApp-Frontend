fb560ea38553267ae3add1fa1766fbb180e088de
rstubryan <restubumirr@gmail.com>
2024-06-15 16:44:56 +0700

    !feat(!update-images*ERROR): update images still error, the rest are fine

--- src/components/organisms/EditQuestionForm/index.jsx
+++ src/components/organisms/EditQuestionForm/index.jsx
@@ -3,9 +3,16 @@ import InputForm from "../../molecules/InputForm/index.jsx";
 import Button from "../../atoms/Button/index.jsx";
 import { getQuestionById, updateQuestion } from "../../../api/questionApi";
 import { useParams } from "react-router-dom";
+import { getTopics } from "../../../api/topicApi.js";
 
 export default function EditQuestionForm() {
   const { id } = useParams();
+  const [topics, setTopics] = useState([]);
+  const [questionImage, setQuestionImage] = useState(null);
+  const [keepCurrentImage, setKeepCurrentImage] = useState(false);
+  const handleCheckboxChange = (event) => {
+    setKeepCurrentImage(event.target.checked);
+  };
 
   const [formValues, setFormValues] = useState({
     title: "",
@@ -14,24 +21,6 @@ export default function EditQuestionForm() {
     topic: "",
   });
 
-  useEffect(() => {
-    async function fetchQuestion() {
-      try {
-        const questionData = await getQuestionById(id);
-        setFormValues({
-          title: questionData.title,
-          body: questionData.body,
-          image: questionData.image,
-          topic: questionData.topic.name,
-        });
-      } catch (error) {
-        console.error("Error fetching question:", error);
-      }
-    }
-
-    fetchQuestion();
-  }, [id]);
-
   const handleChange = (event) => {
     setFormValues({
       ...formValues,
@@ -39,16 +28,49 @@ export default function EditQuestionForm() {
     });
   };
 
+  const handleTopicChange = (topic) => {
+    setFormValues({
+      ...formValues,
+      topic: topic.name,
+    });
+  };
+
+  useEffect(() => {
+    async function fetchQuestionAndTopics() {
+      try {
+        const questionData = await getQuestionById(id);
+        const topicsData = await getTopics();
+        setFormValues((prevFormValues) => ({
+          ...prevFormValues,
+          title: questionData.title,
+          body: questionData.body,
+          image: questionData.image || prevFormValues.image,
+          topic: questionData.topic?.name,
+        }));
+        setTopics(topicsData);
+      } catch (error) {
+        console.error("Error fetching question:", error);
+      }
+    }
+
+    fetchQuestionAndTopics();
+  }, [id]);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
+    const fileInput = document.getElementById("image");
+    const image = !keepCurrentImage ? fileInput.files[0] : undefined;
 
     try {
       const questionData = {
         title: formValues.title,
         body: formValues.body,
-        image: formValues.image,
         topic: formValues.topic,
       };
+
+      if (image) {
+        questionData.image = image;
+      }
 
       const formData = await updateQuestion(id, questionData);
       console.log("Question updated:", formData);
@@ -58,59 +80,108 @@ export default function EditQuestionForm() {
     }
   };
 
+  useEffect(() => {
+    async function fetchImages() {
+      try {
+        const questionImage = await getQuestionById(id);
+        setQuestionImage(questionImage);
+      } catch (error) {
+        console.error("Error fetching images:", error);
+      }
+    }
+
+    fetchImages();
+  }, [id]);
+
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <InputForm
-          htmlFor={"title"}
-          id={"title"}
-          name={"title"}
-          label={"Question Title"}
-          type={"text"}
-          placeholder={"Your question title"}
-          className={"text-body"}
-          value={formValues.title}
-          onChange={handleChange}
-        />
-        <InputForm
-          htmlFor={"body"}
-          id={"body"}
-          name={"body"}
-          label={"Description"}
-          type={"text"}
-          placeholder={"Your description"}
-          className={"text-body"}
-          value={formValues.body}
-          onChange={handleChange}
-        />
-        <InputForm
-          htmlFor={"image"}
-          id={"image"}
-          name={"image"}
-          label={"Image"}
-          type={"file"}
-          placeholder={"Upload your image"}
-          className={"text-body"}
-          value={formValues.image}
-          onChange={handleChange}
-        />
-        <InputForm
-          htmlFor={"topic"}
-          id={"topic"}
-          name={"topic"}
-          label={"Topics"}
-          type={"select"}
-          placeholder={"Topics"}
-          className={"mb-3 text-body"}
-          value={formValues.topic}
-          onChange={handleChange}
-        />
-        <Button
-          variant={"primary"}
-          type={"submit"}
-          children={"Submit"}
-          className="mt-1 w-100 rounded-3 mb-4"
-        />
+        <label>Current Image:</label>
+        <div className="d-lg-flex">
+          <div className="mb-3 w-100 w-lg-25">
+            {questionImage && (
+              <img
+                src={questionImage.imageUrl}
+                alt="Question"
+                className="img-fluid py-3"
+                height={200}
+                width={200}
+              />
+            )}
+            <div>
+              <label className="d-flex gap-2 align-items-center m-0">
+                <input
+                  type="checkbox"
+                  checked={keepCurrentImage}
+                  onChange={handleCheckboxChange}
+                />
+                Keep current image
+              </label>
+            </div>
+          </div>
+          <div className="w-100">
+            <InputForm
+              htmlFor={"topicDropdown"}
+              id={"topicDropdown"}
+              name={"topicDropdown"}
+              label={"Topics"}
+              type={"text"}
+              placeholder={"Select a topic"}
+              className={"text-body w-100 mb-2"}
+              options={topics}
+              onOptionChange={handleTopicChange}
+            />
+            <InputForm
+              htmlFor={"topic"}
+              id={"topic"}
+              name={"topic"}
+              label={"Selected Topic"}
+              type={"text"}
+              placeholder={"Selected topic will appear here"}
+              className={"text-body w-100 mb-3"}
+              value={formValues.topic}
+              readOnly={true}
+            />
+            <InputForm
+              htmlFor={"title"}
+              id={"title"}
+              name={"title"}
+              label={"Question Title"}
+              type={"text"}
+              placeholder={"Your question title"}
+              className={"text-body"}
+              value={formValues.title}
+              onChange={handleChange}
+            />
+            <InputForm
+              htmlFor={"body"}
+              id={"body"}
+              name={"body"}
+              label={"Description"}
+              type={"text"}
+              placeholder={"Your description"}
+              className={"text-body"}
+              value={formValues.body}
+              onChange={handleChange}
+            />
+            <InputForm
+              htmlFor={"image"}
+              id={"image"}
+              name={"image"}
+              label={"Image"}
+              type={"file"}
+              placeholder={"Upload your image"}
+              className={"text-body"}
+              onChange={handleChange}
+            />
+            <Button
+              variant={"primary"}
+              type={"submit"}
+              children={"Submit"}
+              className="mt-1 w-100 rounded-3 mb-4"
+            />
+          </div>
+        </div>
       </form>
     </>
   );
