0c0190bfc14dc0b7e56a3a0d4395a6f265fc866f
rstubryan <restubumirr@gmail.com>
2024-06-16 14:36:33 +0700

    feat(!comment-votes): add toast messages

--- src/components/organisms/CommentVote/index.jsx
+++ src/components/organisms/CommentVote/index.jsx
@@ -7,6 +7,7 @@ import {
 import Button from "../../atoms/Button/index.jsx";
 import IconPlaceholder from "../../atoms/IconPlaceholder/index.jsx";
 import Cookies from "js-cookie";
+import Toasts from "../../molecules/Toasts/index.jsx";
 
 export default function CommentVote({
   commentId,
@@ -19,6 +20,12 @@ export default function CommentVote({
   );
   const [upVotesComments, setUpVotesComments] = useState(0);
   const [downVotesComments, setDownVotesComments] = useState(0);
+  const [showUpvoteSuccessToast, setShowUpvoteSuccessToast] = useState(false);
+  const [showUpvoteFailureToast, setShowUpvoteFailureToast] = useState(false);
+  const [showDownvoteSuccessToast, setShowDownvoteSuccessToast] =
+    useState(false);
+  const [showDownvoteFailureToast, setShowDownvoteFailureToast] =
+    useState(false);
   const user = Cookies.get("user");
   const hasFetchedVotes = useRef(false);
 
@@ -70,15 +77,21 @@ export default function CommentVote({
       if (upvoteResponse.role === "VOTE") {
         setUpvoteSuccessful(true);
         setUpVotesComments(upVotesComments + 1);
+        setShowUpvoteSuccessToast(true);
+        setTimeout(() => setShowUpvoteSuccessToast(false), 3000);
+        window.location.reload();
       } else if (upvoteResponse.message === "VOTE removed") {
         setUpvoteSuccessful(false);
         if (upVotesComments > 0) {
           setUpVotesComments(upVotesComments - 1);
         }
+        setShowUpvoteSuccessToast(true);
+        setTimeout(() => setShowUpvoteSuccessToast(false), 3000);
       }
-      window.location.reload();
     } catch (error) {
       console.error("Error upvoting comment:", error);
+      setShowUpvoteFailureToast(true);
+      setTimeout(() => setShowUpvoteFailureToast(false), 3000);
     }
   };
 
@@ -99,34 +112,82 @@ export default function CommentVote({
       if (downvoteResponse.role === "DOWNVOTE") {
         setDownvoteSuccessful(true);
         setDownVotesComments(downVotesComments + 1);
+        setShowDownvoteSuccessToast(true);
+        setTimeout(() => setShowDownvoteSuccessToast(false), 3000);
+        window.location.reload();
       } else if (downvoteResponse.message === "DOWNVOTE removed") {
         setDownvoteSuccessful(false);
         if (downVotesComments > 0) {
           setDownVotesComments(downVotesComments - 1);
         }
+        setShowDownvoteSuccessToast(true);
+        setTimeout(() => setShowDownvoteSuccessToast(false), 3000);
       }
-      window.location.reload();
     } catch (error) {
       console.error("Error downvoting comment:", error);
+      setShowDownvoteFailureToast(true);
+      setTimeout(() => setShowDownvoteFailureToast(false), 3000);
     }
   };
 
   return (
-    <div className="d-flex gap-2 align-items-center m-0 mt-2">
-      <Button
-        variant={upvoteSuccessful ? "success" : "primary"}
-        className={"w-100 w-md-auto rounded-3"}
-        onClick={handleUpvoteComment}
-      >
-        <IconPlaceholder variant={"arrow-up"} />
-      </Button>
-      <Button
-        variant={downvoteSuccessful ? "danger" : "primary"}
-        className="w-100 w-md-auto rounded-3"
-        onClick={handleDownvoteComment}
-      >
-        <IconPlaceholder variant={"arrow-down"} />
-      </Button>
-    </div>
+    <>
+      <div className="d-flex gap-2 align-items-center m-0 mt-2">
+        <Button
+          variant={upvoteSuccessful ? "success" : "primary"}
+          className={"w-100 w-md-auto rounded-3"}
+          onClick={handleUpvoteComment}
+        >
+          <IconPlaceholder variant={"arrow-up"} />
+        </Button>
+        <Button
+          variant={downvoteSuccessful ? "danger" : "primary"}
+          className="w-100 w-md-auto rounded-3"
+          onClick={handleDownvoteComment}
+        >
+          <IconPlaceholder variant={"arrow-down"} />
+        </Button>
+      </div>
+      {showUpvoteSuccessToast && (
+        <Toasts
+          onClose={() => setShowUpvoteSuccessToast(false)}
+          variant={"success"}
+          variantBody={"success-subtle"}
+          title={"Success"}
+          titleColor={"white"}
+          description={"Upvote successful."}
+        />
+      )}
+      {showUpvoteFailureToast && (
+        <Toasts
+          onClose={() => setShowUpvoteFailureToast(false)}
+          variant={"danger"}
+          variantBody={"danger-subtle"}
+          title={"Failure"}
+          titleColor={"white"}
+          description={"Failed to upvote. Please log in to upvote."}
+        />
+      )}
+      {showDownvoteSuccessToast && (
+        <Toasts
+          onClose={() => setShowDownvoteSuccessToast(false)}
+          variant={"success"}
+          variantBody={"success-subtle"}
+          title={"Success"}
+          titleColor={"white"}
+          description={"Downvote successful."}
+        />
+      )}
+      {showDownvoteFailureToast && (
+        <Toasts
+          onClose={() => setShowDownvoteFailureToast(false)}
+          variant={"danger"}
+          variantBody={"danger-subtle"}
+          title={"Failure"}
+          titleColor={"white"}
+          description={"Failed to downvote. Please log in to downvote."}
+        />
+      )}
+    </>
   );
 }
