4069ac7712eb710cf872f92665d892e6071c921e
rstubryan <restubumirr@gmail.com>
2024-06-18 20:18:57 +0700

    feat(pagination-sorting): add pagination and sorting on topic pages

--- src/components/templates/SinglePostTopicPagesLayout.jsx
+++ src/components/templates/SinglePostTopicPagesLayout.jsx
@@ -11,6 +11,7 @@ import Card from "../molecules/Card/index.jsx";
 import Sidebar from "../molecules/Sidebar/index.jsx";
 import CardHeader from "../organisms/CardHeader/index.jsx";
 import TypographyText from "../atoms/TypographyText/index.jsx";
+import NavTabs from "../molecules/NavTabs/index.jsx";
 
 export default function SinglePostTopicPagesLayout() {
   const [questions, setQuestions] = useState([]);
@@ -19,6 +20,10 @@ export default function SinglePostTopicPagesLayout() {
   const [votesData, setVotesData] = useState({});
   const { id } = useParams();
   const fetchDataRef = useRef(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [questionsPerPage] = useState(5);
+  const [sortOrder, setSortOrder] = useState("latest");
+  const [sortedQuestions, setSortedQuestions] = useState([]);
 
   useEffect(() => {
     if (!fetchDataRef.current) {
@@ -54,6 +59,49 @@ export default function SinglePostTopicPagesLayout() {
     }
   }, [id]);
 
+  const filteredQuestions = sortedQuestions.filter(
+    (question) => question.forumID === null,
+  );
+  const indexOfLastQuestion = currentPage * questionsPerPage;
+  const indexOfFirstQuestion = indexOfLastQuestion - questionsPerPage;
+  const currentQuestions = filteredQuestions.slice(
+    indexOfFirstQuestion,
+    indexOfLastQuestion,
+  );
+  const maxPageNumbersToShow = 5;
+  const totalPages = Math.ceil(filteredQuestions.length / questionsPerPage);
+  let startPage = Math.max(
+    currentPage - Math.floor(maxPageNumbersToShow / 2),
+    1,
+  );
+  let endPage = Math.min(startPage + maxPageNumbersToShow - 1, totalPages);
+  if (endPage - startPage + 1 < maxPageNumbersToShow && startPage > 1) {
+    startPage = Math.max(endPage - maxPageNumbersToShow + 1, 1);
+  }
+
+  const handleSortOrderChange = (tabId, event) => {
+    event.preventDefault();
+    if (tabId === "tab1") {
+      setSortOrder("oldest");
+    } else if (tabId === "tab2") {
+      setSortOrder("latest");
+    }
+    setCurrentPage(1);
+  };
+
+  useEffect(() => {
+    const newSortedQuestions = [...questions];
+    newSortedQuestions.sort((a, b) => {
+      if (sortOrder === "latest") {
+        return new Date(b.createdAt) - new Date(a.createdAt);
+      } else {
+        return new Date(a.createdAt) - new Date(b.createdAt);
+      }
+    });
+
+    setSortedQuestions(newSortedQuestions);
+  }, [sortOrder, questions]);
+
   return (
     <>
       <PagesLayout>
@@ -70,38 +118,41 @@ export default function SinglePostTopicPagesLayout() {
                   title={topic.name}
                   description={
                     <>
-                      <TypographyText>
-                        Total questions:{" "}
-                        {
-                          questions.filter(
+                      <div className="">
+                        <TypographyText>
+                          Total questions:{" "}
+                          {
+                            questions.filter(
+                              (question) => question.forumID === null,
+                            ).length
+                          }
+                        </TypographyText>
+                        <TypographyText>
+                          Oldest question:{" "}
+                          {questions.filter(
                             (question) => question.forumID === null,
-                          ).length
-                        }
-                      </TypographyText>
-                      <TypographyText>
-                        Oldest question:{" "}
-                        {questions.filter(
-                          (question) => question.forumID === null,
-                        ).length > 0
-                          ? questions.filter(
-                              (question) => question.forumID === null,
-                            )[0].title
-                          : "No questions found"}
-                      </TypographyText>
-                      <TypographyText>
-                        Newest question:{" "}
-                        {questions.filter(
-                          (question) => question.forumID === null,
-                        ).length > 0
-                          ? questions.filter(
-                              (question) => question.forumID === null,
-                            )[
-                              questions.filter(
+                          ).length > 0
+                            ? questions.filter(
                                 (question) => question.forumID === null,
-                              ).length - 1
-                            ].title
-                          : "No questions found"}
-                      </TypographyText>
+                              )[0].title
+                            : "No questions found"}
+                        </TypographyText>
+                        <TypographyText>
+                          Newest question:{" "}
+                          {questions.filter(
+                            (question) => question.forumID === null,
+                          ).length > 0
+                            ? questions.filter(
+                                (question) => question.forumID === null,
+                              )[
+                                questions.filter(
+                                  (question) => question.forumID === null,
+                                ).length - 1
+                              ].title
+                            : "No questions found"}
+                        </TypographyText>
+                        <NavTabs onTabClick={handleSortOrderChange} />
+                      </div>
                     </>
                   }
                   buttonTitle={"Ask a Question"}
@@ -112,8 +163,8 @@ export default function SinglePostTopicPagesLayout() {
             <div className="col-12 col-lg-10 ms-auto mb-4">
               {topic ? (
                 <>
-                  {questions.length > 0 ? (
-                    questions
+                  {currentQuestions.length > 0 ? (
+                    currentQuestions
                       .filter((question) => question.forumID === null)
                       .map((question) => (
                         <CardPost
@@ -158,6 +209,55 @@ export default function SinglePostTopicPagesLayout() {
               ) : (
                 <p>Loading...</p>
               )}
+              <div className="justify-content-center d-flex">
+                <ul className="pagination">
+                  <li className="page-item">
+                    <a
+                      role="button"
+                      className="page-link"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        if (currentPage > 1) {
+                          setCurrentPage(currentPage - 1);
+                        }
+                      }}
+                    >
+                      Previous
+                    </a>
+                  </li>
+                  {Array(endPage - startPage + 1)
+                    .fill()
+                    .map((_, index) => (
+                      <li key={index + startPage} className={`page-item`}>
+                        <a
+                          role="button"
+                          className={`page-link ${currentPage === index + startPage ? "bg-primary-subtle text-body" : ""}`}
+                          onClick={(e) => {
+                            e.preventDefault();
+                            setCurrentPage(index + startPage);
+                          }}
+                        >
+                          {index + startPage}
+                        </a>
+                      </li>
+                    ))}
+                  <li className="page-item">
+                    <a
+                      role="button"
+                      className="page-link"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        if (currentPage < totalPages) {
+                          setCurrentPage(currentPage + 1);
+                        }
+                      }}
+                      disabled={currentPage === totalPages}
+                    >
+                      Next
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </ContainerLayout>
