e23952457ad16652dea1b5ab2686813e2f6df160
rstubryan <restubumirr@gmail.com>
2024-06-19 12:21:40 +0700

    feat(main-pages): add main pages design

--- src/components/templates/MainPagesLayout.jsx
+++ src/components/templates/MainPagesLayout.jsx
@@ -4,8 +4,54 @@ import ContainerLayout from "./ContainerLayout.jsx";
 import PagesLayout from "./PagesLayout.jsx";
 import CardHeader from "../organisms/CardHeader/index.jsx";
 import CardPost from "../organisms/CardPost/index.jsx";
+import { useEffect, useState } from "react";
+import { getQuestions } from "../../api/questionApi.js";
+import { getCommentsByPostId } from "../../api/commentApi.js";
+import { getVotes } from "../../api/voteApi.js";
+import { Link } from "react-router-dom";
 
 export default function MainPagesLayout() {
+  const [questions, setQuestions] = useState([]);
+  const [comments, setComments] = useState({});
+  const [votesData, setVotesData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchQuestionsAndComments() {
+      try {
+        let questions = await getQuestions();
+        const comments = {};
+        const votesData = {};
+        questions = questions.filter(
+          (question) =>
+            question.createdBy.uuid === "clxbwkys80001jp9e005w5gwz" ||
+            question.createdBy.username.toLowerCase() === "twenties",
+        );
+
+        for (let question of questions) {
+          comments[question.uuid] = await getCommentsByPostId(question.uuid);
+          let votes = await getVotes(question.uuid);
+          let upVotes = votes.filter((vote) => vote.role === "VOTE");
+          let downVotes = votes.filter((vote) => vote.role === "DOWNVOTE");
+
+          votesData[question.uuid] = {
+            votes: upVotes.length,
+            downVotes: downVotes.length,
+          };
+        }
+        setQuestions(questions);
+        setComments(comments);
+        setVotesData(votesData);
+        setLoading(false);
+        console.log("Votes data:", votesData);
+      } catch (error) {
+        console.error("Error fetching questions and comments:", error);
+        setLoading(false);
+      }
+    }
+    fetchQuestionsAndComments();
+  }, []);
+
   return (
     <>
       <PagesLayout>
@@ -27,14 +73,31 @@ export default function MainPagesLayout() {
               />
             </div>
             <div className="col-12 col-lg-10 ms-auto mb-3">
-              <CardPost>
-                title={"Latest Questions"}
-                description={"Here are the latest questions from our users"}
-                author={"Twenties"}
-                date={"2021-09-01"}
-                votes={"10"}
-                comments={"5"}
-              </CardPost>
+              {questions.map((question) => (
+                <CardPost
+                  key={question.uuid}
+                  topic={question.topic?.name}
+                  title={
+                    <Link
+                      to={`/question/${question.uuid}`}
+                      className="text-decoration-none"
+                    >
+                      {question.title}
+                    </Link>
+                  }
+                  showImage={false}
+                  showButtons={false}
+                  description={question.body}
+                  createdAt={new Date(question.createdAt).toLocaleString()}
+                  username={question.createdBy.username}
+                  avatarSrc={question.createdBy.avatar}
+                  avatarAlt={question.createdBy.username}
+                  votes={votesData[question.uuid]?.votes || 0}
+                  downvotes={votesData[question.uuid]?.downVotes || 0}
+                  answers={comments[question.uuid]?.length || 0}
+                  className={"mb-3"}
+                />
+              ))}
             </div>
           </div>
         </ContainerLayout>
